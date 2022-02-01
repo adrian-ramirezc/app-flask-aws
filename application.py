10719c0a5031bfc5e08fa9eb462dc1f86102951e
Adrian RAMIREZ <adrian.ramirez.caldas@gmail.com>
2022-02-01 12:48:05 +0100

    env / flask app created

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask 
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return 'Hey there'
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
